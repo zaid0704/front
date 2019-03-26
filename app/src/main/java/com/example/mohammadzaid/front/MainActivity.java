5a8cccbbe1a3ea52d8c3444f52c91fdab2ad09eb
zaid0704 <mohammadzaid0704@gmail.com>
2019-03-26 16:16:25 +0530

    Front

---
+++ app/src/main/java/com/example/mohammadzaid/front/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.mohammadzaid.front;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    private DrawerLayout drawer ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        android.support.v7.widget.Toolbar toolbar =findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        ActionBarDrawerToggle toggle =new ActionBarDrawerToggle(this,drawer,toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+    toggle.syncState();
+    }
+    public boolean onNavigationItemSelected(@NonNull MenuItem item)
+    {
+        switch (item.getItemId())
+        {
+            case R.id.nav_gmail:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragmnet_container,new GmailFragment()).commit ();
+                break;
+            case R.id.nav_profile:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragmnet_container,new ProfileFragment()).commit ();
+                break;
+            case R.id.nav_pn:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragmnet_container,new PhoneFragment()).commit ();
+                break;
+            case R.id.settings:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragmnet_container,new SettingFragment()).commit ();
+                break;
+
+        }
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+    @Override
+    public void onBackPressed() {
+        if(drawer.isDrawerOpen(GravityCompat.START))
+            drawer.closeDrawer(GravityCompat.START);
+        else
+            super.onBackPressed();
+        super.onBackPressed();
+    }
+}
